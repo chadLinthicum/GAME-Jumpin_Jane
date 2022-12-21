0687f0d38ddc986e7efdd759125721dc2b00a936
Chad Linthicum <portableanswers@gmail.com>
2022-12-20 19:43:38 -0500

    - Particle effects added for running and game over collision - Audio added - background music, jumping, game over - Fixed a bug causing reoccuring collision on game over

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
     private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip explosionAudio;
+    public AudioClip jumpAudio;
+    public AudioClip gameOverAudio;
     public float jumpForce = 10;
     public float gravityModifier = 1;
     public bool isOnGround = true;
@@ -14,29 +20,35 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Physics.gravity *= gravityModifier;
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
-        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             // ForceMode.Impulse immediately applies the force and needs less force
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpAudio, 1);
         }
     }
 
     // Whenever player enters a collision with something
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") && !gameOver)
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         if (collision.gameObject.CompareTag("Obstacle"))
         {
@@ -44,6 +56,12 @@ public class PlayerController : MonoBehaviour
             Debug.Log("GAME OVER");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(explosionAudio, 0.5f);
+            playerAudio.PlayOneShot(gameOverAudio, 1);
+            // Knock player back to prevent reoccurring collisions
+            transform.Translate(Vector3.back * 0.5f * Time.deltaTime * 50);
         }
 
 
